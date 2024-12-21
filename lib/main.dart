66b06a2c96acee630050045496dca36b253891b6
pparnfaa <kumpol.sb@gmail.com>
2024-12-21 16:00:03 +0700

    create container

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Flutter Demo',
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: const Icon(Icons.add),
+            backgroundColor: Colors.purple,
+          ),
+          body: Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.all(20),
+            child: const Text('Hello'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("Hello");
+            },
+            child: const Icon(Icons.phone),
+          ),
+        ));
   }
 }
-
