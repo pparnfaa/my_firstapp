6084fc4817aa732957de6ac1a482ac770c0b6dc5
pparnfaa <kumpol.sb@gmail.com>
2025-01-04 09:00:16 +0700

    assets png

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
           body: Center(
             // child : Image.network('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSaXYTlyWcEBs-yQ_ll_j43p-ySqRizPFM0Q_nb1UNF-MZJEwFnXthK4HGv9fIuvUcNVKFZ2bzH1ESf-BPLBzE6B5IasTeJHxaf-lXkdw'),
            child: Image.asset(
-            'assets/IMG_2477.jpg',
+            'assets/cat.png',
             width: 300,
            ),
            // child: ElevatedButton(
