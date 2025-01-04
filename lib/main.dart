62b9129fd20309a6146192dedba6d32512d2bf9b
pparnfaa <kumpol.sb@gmail.com>
2025-01-04 11:54:55 +0700

    Statefull Counter

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const GreetingWidget(name: 'Parnfa',));
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyWidget(),
+    );
   }
 }
 
-class GreetingWidget extends StatelessWidget {
-  final String name; 
-  const GreetingWidget({super.key, required this.name});
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Demo App'),
+      appBar: AppBar(
+        title: const Text('Demo App'),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Counter",
+              style: const TextStyle(
+                  fontSize: 24, 
+                  color: Colors.black,
+              ),
+            ),
+            Text(
+              "$counter",
+              style: const TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                    child: Icon(Icons.add)
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if(counter > 0){
+                          counter--;
+                        }
+                        else{
+                          counter = 0;
+                        }
+                      });
+                    },
+                    child: Icon(Icons.remove)
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        counter = 0;
+                      });
+                    },
+                    child: Icon(Icons.restart_alt)
+                ),
+              ],
+            )
+          ],
         ),
-         body: Center( 
-            child: Text( 
-      'Hello, $name', 
-        style: const TextStyle(fontSize: 20),
-        )));
+      ),
+    );
   }
 }
